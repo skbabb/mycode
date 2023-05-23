11a6918d416f3fd1bca125a49f0fd3e8ef8dad7c
skbabb <shaquille.k.babb@gmail.com>
2023-05-23 18:22:58 +0000

    Join and split

---
+++ challenge13.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+print("what is your name? ")
+
+name = input("enter your name: ")
+
+print("what day of the week is it?: ")
+
+day = input("enter the day of the week: ")
+
+
+# You should choose either the , or the +
+# notice the comma is part of the string now.
+# Now, you can use the + to "add" two strings together
+# We're getting closer.  
+# Now we just need the final !  
+# We can use + to concatenate it meaning it's just crammed right against the string before
+
+
+print("Hello," + name + "!  " + "Happy " + day + "!")
+
+# These are tricky.  Hopefully, that works.
